21f1bae3205f38aea55ed0c81476ed2477a30bd4
armen231 <armen.mowsesyan2017@yandex.ru>
2024-02-24 17:06:54 +0500

    Создание maven проекта

---
+++ LaboratoryWork2Maven/src/test/java/LaboratoryWork2/LaboratoryWork2Maven/AppTest.java
@@ -0,0 +1,20 @@
+package LaboratoryWork2.LaboratoryWork2Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
